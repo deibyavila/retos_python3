4f0b26d4e123419714d11c6befd2f6d2d1e754e7
=Deiby Avila <deibyavila@gmail.com>
2020-02-24 15:28:01 -0500

    solucion reto busqueda de cuantas veces se repite en la cadena original

--- search sub_strin on string.py
+++ search sub_strin on string.py
@@ -1,17 +1,11 @@
 
-
 def count_substring(string, sub_string):
-    count = 0
-    if 1 <= len(string) <= 200:
-        for i in range(0, len(string)):
-            if string.find("sub_string") != -1:
-                count += 1
+    count =(sum([ 10 for i in range(len(string)-len(sub_string)+1) 
+                        if string[i:i+len(sub_string)] == sub_string]
+                        ))
+    return print(count)
 
-    return count
 
-if __name__ == '__main__':
-    string = input().strip()
-    sub_string = input().strip()
-    
-    count = count_substring(string, sub_string)
-    print(count)
+
+count_substring("jaajaja", "a")
+
