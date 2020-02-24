c823422f1106dc73cd4d49801ec5c95640c927bb
=Deiby Avila <deibyavila@gmail.com>
2020-02-24 15:52:27 -0500

    reto , determinar si el año es visiesto para el calendario gregoriano

---
+++ loops.py
@@ -0,0 +1,10 @@
+"""
+Read an integer . For all non-negative integers , print . See the sample for details.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+    if 1<= n <= 20:
+        for i in range(0,n):
+            print(i**2)
